bb64a561151017b16ea746fd87ee738ca44a2dfd
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-20 10:21:58 -0500

    Update

---
+++ functions/commands/fun/cola.js
@@ -0,0 +1,22 @@
+const { sleep } = require('../../basic'); 
+const { prefix, owner, maid, dogwater } = require('../../../config/config.json');
+
+module.exports = {
+    name: "givecola",
+    category: "fun",
+    description: "Gives someone (or yourself) cola",
+    aliases: ["cola"],
+    usage: "[mention]",
+    run: async (client, msg, args) => {
+
+        if (args.length == 0) {
+            msg.author.send({ files: ["./files/videos/cola.mp4"]})
+        } else {
+            if (msg.mentions.members.first()) {
+                let user = msg.mentions.members.first()
+
+                user.send({ files: ["./files/videos/cola.mp4"]})
+            }
+        }
+    }
+}
