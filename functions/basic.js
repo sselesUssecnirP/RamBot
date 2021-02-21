aa7980391ebd0beae849ffd8ce8367c91779a4a2
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-20 21:46:24 -0500

    Update

--- functions/basic.js
+++ functions/basic.js
@@ -1,12 +1,17 @@
 module.exports = {
-    sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    },
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  },
 
-    formatDate(date) {
-      return date.toLocaleString('en-US', { timeZone: 'America/New_York' })
-    },
-    mentionUser(user) {
-      return `<@!${user}>`
-    }
+  formatDate(date) {
+    date.toLocaleString('en-US', { timeZone: 'America/New_York' })
+
+    return date[1]
+  },
+  formatDateTime(date) {
+    return date.toLocaleString('en-US', { timeZone: 'America/New_York' })
+  },
+  mentionUser(user) {
+    return `<@!${user}>`
+  }
 }
