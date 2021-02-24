0bcfead1355ba4e0ce2356ae369c1989ff190189
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-24 13:17:57 -0500

    say.js update

--- functions/commands/fun/say.js
+++ functions/commands/fun/say.js
@@ -4,10 +4,10 @@ const { prefix, master, maid, dogwater } = require('../../../config/config.json'
 
 module.exports = {
     name: "say",
-    category: "info",
+    category: "fun",
     description: "Creates an embed with your own message.",
     aliases: [],
-    usage: "<MESSAGE> [hexColor] [channel_ID | channel_MENTION]\nFor <MESSAGE>, write it like this: `Hi,there,people,how,are,you?`. Using commas INSTEAD of space.",
+    usage: "<MESSAGE> [hexColor] [channel_ID | channel_MENTION]\nFor <MESSAGE>, write it like this: `Hi\`there\`people\`how\`are\`you?`. Using commas INSTEAD of space.",
     run: async (client, msg, args) => {
         
         let emilia = await msg.guild.members.cache.get('765440066495184896')
@@ -28,33 +28,17 @@ module.exports = {
         let customChannel;
 
         if (customColor)
-            args[1].includes('#') ? null : customColor = false
+            args[1].includes('#') && !args[1].includes('<#!') ? null : customColor = false
 
         if (customColor)
-            customChannel = args[2] ? true : false
-
-        if (!customColor)
-            customChannel = args[1] ? true : false
-
-        if (customColor && !customChannel) {
-            if (msg.mentions.channels.first()) {
-                customColor = false
-                customChannel = true
-            }
-        }
-
-        if (customColor) {
             color = args[1]
-            args.slice(args[1])
-        }
 
-        if (customChannel) {
+
+        if (customChannel)
             channel = msg.mentions.channels.first() || msg.guild.channels.get(args[1])
-            args.slice(args[1])
-        }
 
-        args = args.filter(i => i == args[1] || args[2])
-        args = args[0].split(',').join(' ')
+        args = args.filter(i => i !== args[1] || args[2])
+        args = args[0].split(/ [^`]`[^`]/ && / [^`]`/ && / `[^`]/).join(' ')
 
         let embed = new MessageEmbed()
             .setAuthor(msg.member.displayName, msg.author.displayAvatarURL())
