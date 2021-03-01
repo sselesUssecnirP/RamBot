2db18424fce86ac18bdc7a6e67c59a6b1be8db20
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-03-01 00:34:05 -0500

    Update serverinfo.js

--- functions/commands/info/serverinfo.js
+++ functions/commands/info/serverinfo.js
@@ -13,46 +13,48 @@ module.exports = {
     if (msg.channel.type == 'DM')
     return msg.reply("This is a direct message. You cannot get information about the server you're in from here.")
 
-    let guild = msg.guild
-
     const joined = formatDate(msg.member.joinedAt);
 
-    const roles = guild.roles.cache
-        .filter(r => r.name != 'everyone')
-        .map(r => r)
-        .join(", ") || "none"
+    const roles = [];
     
-    const created = formatDate(guild.createdAt)
+    await msg.guild.roles.cache.each(r => {
+        if (roles.name != '@everyone')
+            roles.push(`<@!${r.id}>`)
+    });
+
+    roles.join(', ')
+    
+    const created = formatDate(msg.guild.createdAt)
 
     const embed = new MessageEmbed()
         .setTitle('Serverinfo')
-        .setFooter(guild.name, guild.iconURL)
-        .setThumbnail(guild.iconURL)
+        .setFooter(msg.guild.name, msg.guild.iconURL)
+        .setThumbnail(msg.guild.iconURL)
         .setColor(msg.member.displayHexColor === "#000000" ? "#ffffff" : msg.member.displayHexColor)
 
-        .addField("Guild Information #1", stripIndents`**> Name:** ${guild.name}
-        **>> ID:** ${guild.id}
+        .addField("Guild Information #1", stripIndents`**> Name:** ${msg.guild.name}
+        **>> ID:** ${msg.guild.id}
         **>> Created At:** ${created}
-        **>> Description:** ${guild.id}`, true)
+        **>> Description:** ${msg.guild.id}`, true)
 
         .addField("Roles", stripIndents`**>> Roles:** ${roles}`)
         
-        .addField("Info #2", stripIndents`${guild.rulesChannel ? `
-        **>> Rules Channel:** <#!${guild.rulesChannelID}>` : ``}
-        **>> Population:** ${guild.membersCount}
+        .addField("Info #2", stripIndents`${msg.guild.rulesChannel ? `
+        **>> Rules Channel:** <#!${msg.guild.rulesChannelID}>` : ``}
+        **>> Population:** ${msg.guild.membersCount}
         **>> Joined At:** ${joined}
-        **>> Voice Region:** ${guild.region}`, true)
+        **>> Voice Region:** ${msg.guild.region}`, true)
 
-        .addField("Info #3", stripIndents`${guild.vanityURLCode ? `
-        **>> Vanity URL:** ${guild.vanityURLCode}
-        **>> Vanity URL Uses:** ${guild.vanityURLUses}` : ``}${guild.partnered ? `
-        **>> Partnered:** Yes` : ``}${guild.verified ? `
+        .addField("Info #3", stripIndents`${msg.guild.vanityURLCode ? `
+        **>> Vanity URL:** ${msg.guild.vanityURLCode}
+        **>> Vanity URL Uses:** ${msg.guild.vanityURLUses}` : ``}${msg.guild.partnered ? `
+        **>> Partnered:** Yes` : ``}${msg.guild.verified ? `
         **>> Verified:** Yes` : ``}`, true)
 
         .setTimestamp()
 
-    if (guild.bannerURL) {
-        embed.setImage(guild.bannerURL)
+    if (msg.guild.bannerURL) {
+        embed.setImage(msg.guild.bannerURL)
     }
 
     msg.channel.send("Here's the server information!", embed)
