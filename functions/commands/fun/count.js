df6bef2313b13e894b4627b87701efd70d99b8e7
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-03-13 15:09:20 -0500

    Update count.js

--- functions/commands/fun/count.js
+++ functions/commands/fun/count.js
@@ -2,11 +2,11 @@ const func = require('../../basic');
 const { prefix, master, maid, dogwater } = require('../../../config/config.json');
 
 module.exports = {
-    name: "newcommand",
-    category: "newcommand",
-    description: "newcommand",
-    aliases: ["newcommand"],
-    usage: "none",
+    name: "count",
+    category: "fun",
+    description: "Counts to the number! Use New_Number if you're restarting a count. (i.e last number = 100 so do `ram!count 100 1500 101` ... when restarting counting, you'll need to include a timeout between messages.)",
+    aliases: [],
+    usage: "<Number> [timeout_between_messages] [New_Number]",
     run: async (client, msg, args) => {
         
 
